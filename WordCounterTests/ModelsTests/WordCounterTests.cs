06d263aa46348249be4e76f80cb204e2b4c356db
silvermistshadow <bmead72@gmail.com>
2020-03-20 08:44:27 -0700

    start writing first test, update gitignore because vscode insisted on putting its own folder in

--- WordCounterTests/ModelsTests/WordCounterTests.cs
+++ WordCounterTests/ModelsTests/WordCounterTests.cs
@@ -1,2 +1,19 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using WordCounter;
+
+namespace WordCounterTests
+{
+    [TestClass]
+    public class WordCounterTests
+    {
+        [TestMethod]
+        public void WordCounter_SingleLetterMatch_True()
+        {
+            //Arrange
+            WordCounter.WordCounter testLetter = new WordCounter.WordCounter('a', 'a');
+            //Act
+            
+            //Assert
+        }
+    }
+}
