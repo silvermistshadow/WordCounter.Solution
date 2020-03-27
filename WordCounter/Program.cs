3da33611f1ed0774570d2e99bab334df82a4fe2d
silvermistshadow <bmead72@gmail.com>
2020-03-27 10:02:12 -0700

    write and test UI

---
+++ WordCounter/Program.cs
@@ -0,0 +1,19 @@
+using System;
+using WordCounter.Models;
+
+namespace WordCounter
+{
+    class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("This program takes a word and a sentence and tells how many times a word is repeated.");
+            Console.WriteLine("First, please enter the word:");
+            string inputWord = Console.ReadLine();
+            Console.WriteLine("Next, please enter the sentence:");
+            string inputSentence = Console.ReadLine();
+            RepeatCounter repeat = new RepeatCounter(inputWord, inputSentence);
+            Console.WriteLine("There were {0} matches", repeat.matchCount());
+        }
+    }
+}
